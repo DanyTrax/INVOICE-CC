a0be98182b2da856131535eaa0b6affaad89db87
DanyTrax <danny9203@gmail.com>
2026-01-22 00:20:10 -0500

    fix: Corregir declaración de propiedad view en CalendarWidget (no debe ser static)

--- app/Filament/Widgets/CalendarWidget.php
+++ app/Filament/Widgets/CalendarWidget.php
@@ -7,7 +7,7 @@ use Filament\Widgets\Widget;
 
 class CalendarWidget extends Widget
 {
-    protected static string $view = 'filament.widgets.calendar-widget';
+    protected string $view = 'filament.widgets.calendar-widget';
     
     protected int | string | array $columnSpan = 'full';
     
