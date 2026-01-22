b2e8df17a4debaa4015675183de15100718a1905
DanyTrax <danny9203@gmail.com>
2026-01-22 01:40:18 -0500

    fix: Completar migración de settings con todos los valores por defecto

--- database/settings/2026_01_22_063941_create_general_settings.php
+++ database/settings/2026_01_22_063941_create_general_settings.php
@@ -6,6 +6,21 @@ return new class extends SettingsMigration
 {
     public function up(): void
     {
-
+        $this->migrator->add('general.agency_name', 'RAMS');
+        $this->migrator->add('general.agency_nit', '');
+        $this->migrator->add('general.agency_address', '');
+        $this->migrator->add('general.agency_phone', '');
+        $this->migrator->add('general.agency_email', '');
+        $this->migrator->add('general.agency_website', '');
+        $this->migrator->add('general.agency_logo', '');
+        $this->migrator->add('general.drive_service_account_json', '');
+        $this->migrator->add('general.mail_mailer', 'smtp');
+        $this->migrator->add('general.mail_host', 'smtp.gmail.com');
+        $this->migrator->add('general.mail_port', 587);
+        $this->migrator->add('general.mail_username', '');
+        $this->migrator->add('general.mail_password', '');
+        $this->migrator->add('general.mail_encryption', 'tls');
+        $this->migrator->add('general.mail_from_address', 'noreply@rams.com');
+        $this->migrator->add('general.mail_from_name', 'RAMS Sistema');
     }
 };
